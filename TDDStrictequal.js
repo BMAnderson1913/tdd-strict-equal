40ad9898edd6710fafab1e25564fd40940d2bb63
Brittany Anderson <brogers1913@gmail.com>
2020-03-08 17:07:28 -0400

    Amended testing and added code to TDDStrictequal

---
+++ TDDStrictequal.js
@@ -0,0 +1,5 @@
+const strictEquals = (valNumOne, valNumTwo) => {
+
+}
+
+module.exports = strictEquals
--- tests/TDDStrictequal.test.js
+++ tests/TDDStrictequal.test.js
@@ -1,17 +1,20 @@
 const describe = require('mocha').describe
 const it = require('mocha').it
 const expect = require('chai').expect
-const strictequals = require('../TDDStrictequal')
+const strictEquals = require('../TDDStrictequal')
 
 describe('TDDStrictEqual', () => {
-  describe('strictequals', () => {
+  describe('strictEquals', () => {
     it('Returns true if two values are INEQUAL in TYPE and VALUE', () => {
+      //create test data
       const valNumOne = 3
       const valNumTwo = 'b'
 
+      //call the function using the test data
       const endresult = strictEquals(valNumOne, valNumTwo)
 
-      expect(endresult).to.equal(false)
+      //make assertions about what should be true
+      expect(endresult).to.not.equal(true)
     })
     it('Returns true if two values are EQUAL in VALUE but INEQUAL in TYPE', () => {
       const valNumOne = 3
@@ -19,7 +22,7 @@ describe('TDDStrictEqual', () => {
 
       const endresult = strictEquals(valNumOne, valNumTwo)
 
-      expect(endresult).to.equal(false)
+      expect(endresult).to.not.equal(true)
     })
     it('Returns true if two values are EQUAL in TYPE but INEQUAL in VALUE', () => {
       const valNumOne = 3
@@ -27,7 +30,7 @@ describe('TDDStrictEqual', () => {
 
       const endresult = strictEquals(valNumOne, valNumTwo)
 
-      expect(endresult).to.equal(false)
+      expect(endresult).to.not.equal(true)
     })
     it('Returns true if two values are EQUAL in both TYPE and VALUE', () => {
       const valNumOne = 3
@@ -35,7 +38,7 @@ describe('TDDStrictEqual', () => {
 
       const endresult = strictEquals(valNumOne, valNumTwo)
 
-      expect(endresult).to.equal(true)
+      expect(endresult).to.not.equal(false)
 
     })
   })
